1afef4914738f0f033c52779fe2c84387ffa9dc4
ishaplan <ishaplan@ucsc.edu>
2024-04-17 11:53:00 -0700

    hello

--- examples/hello.cpp
+++ examples/hello.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
-using namespace std;
-
-int main(){
-    cout<< "Hello, World!" << endl;
-    return 0;
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
 }
