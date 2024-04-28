54d3e3109b19651b14e847ce2c8c4c0f0cbc9dfc
Harshil <hgupta2@ucsc.edu>
2024-04-28 15:22:04 -0700

    reading binary

---
+++ src/hardware/memory.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+
+class Memory {
+private:
+    std::unique_ptr<char[]> contents_;
+    std::size_t file_size_;
+public:
+    Memory(const std::string& filename);
+    const char* getData() const;
+    std::size_t getSize() const;
+};
