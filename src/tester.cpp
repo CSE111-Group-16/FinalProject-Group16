13d64407aef03d42964a2ab54e54709090b42263
Jaxon Ruiz <jacruiz@ucsc.edu>
2024-05-01 04:24:27 -0700

    removed debug lines

--- src/tester.cpp
+++ src/tester.cpp
@@ -6,8 +6,6 @@ using namespace std;
 Tester::Tester() {
     std::string path = "../../local-rom/hws/hello_world1.slug";
     OS os;
-    cout<< "attempting os.startup" << endl;
     os.startup(path);
-    cout << "finished testing startup" << endl;
 }
 
