1dd6f5608e0ae2d837765b2b1fc969754993ace5
moot moot <ehluu@ucsc.edu>
2024-05-31 14:22:33 -0700

    main and testing github actions on hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,21 @@
 #include <iostream>
 #include <filesystem>
 #include <SDL2/SDL.h>
-#include "tester.h"
 #include "processes/os.h"
 #include "hardware/CPU/CPU.h"
 #include "hardware/memory.h"
 
 int main(int argc, char* argv[]) {
-	if (2 == argc) {
-		SDL_Init(SDL_INIT_VIDEO);
-		std::string path = argv[1];
-		// https://en.cppreference.com/w/cpp/filesystem/exists
-		// check if the file exists:
-		std::filesystem::path filepath = path;
-		bool filepathExists = std::filesystem::exists(filepath);
-		if (filepathExists) {
-			// check if its a slug file:
-			if (filepath.extension() == ".slug") {
-				OS os;
-				os.startup(path);
-				
-			}
-			else {
-				std::cout << "Provide only a path to a valid slug file" << std::endl;
-			}
-		}
-		else {
-			std::cout << "Provide only a path to a valid slug file" << std::endl;
-		}
-	}
-	else {
-		std::cout << "Provide only a path to a valid slug file" << std::endl;
-	}
-	
+	if (argc != 2) { std::cout << "Provide only a path to a valid slug file" << std::endl; }
+	std::string path = argv[1];
+	// https://en.cppreference.com/w/cpp/filesystem/exists
+	// check if the file exists:
+	std::filesystem::path filepath = path;
+	bool filepathExists = std::filesystem::exists(filepath);
+	if (!filepathExists) { std::cout << "Provide only a path to a valid slug file" << std::endl; }
+	if (filepath.extension() != ".slug") { std::cout << "Provide only a path to a valid slug file" << std::endl; }
+	OS os;
+	os.startup(path);
+	SDL_Init(SDL_INIT_VIDEO);
 	return 0;
 }
